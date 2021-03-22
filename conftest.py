97ded5fedf340edc9d9d3e92f0745910c86e8cc5
alekseytroe <alekseytroe@gmail.com>
2021-03-22 12:19:57 +0300

    изменение значения по умолчанию для параметра язык

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='',
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language")
 
 
